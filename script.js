581edb3b68b58795c4cb2771d1935830b95238c3
TylerS54 <153526130+TylerS54@users.noreply.github.com>
2024-01-03 12:11:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-let counts = {
-    Alice: 0,
-    Bob: 0,
-    Charlie: 0
+var firebaseConfig = {
+    apiKey: "AIzaSyCUCLvljUj90NhC3w_aUU6SwkFvOt-asDk",
+    authDomain: "loglog-a3cf1.firebaseapp.com",
+    databaseURL: "https://loglog-a3cf1-default-rtdb.firebaseio.com",
+    projectId: "loglog-a3cf1",
+    storageBucket: "loglog-a3cf1.appspot.com",
+    messagingSenderId: "1056500947260",
+    appId: "1:1056500947260:web:4635c3a82b4fa2777b2639",
+    measurementId: "G-TGSND834KX"
 };
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
 function incrementCounter(name) {
-    counts[name]++;
-    updateDisplay();
+    var countRef = database.ref('counts/' + name);
+    countRef.transaction(function(currentCount) {
+        return (currentCount || 0) + 1;
+    });
 }
 
 function updateDisplay() {
-    const countsElement = document.getElementById('counts');
-    countsElement.innerHTML = '';
-    for (const name in counts) {
-        countsElement.innerHTML += `<p>${name}: ${counts[name]}</p>`;
-    }
+    var countsElement = document.getElementById('counts');
+    var countsRef = database.ref('counts');
+    countsRef.on('value', function(snapshot) {
+        countsElement.innerHTML = '';
+        snapshot.forEach(function(childSnapshot) {
+            var name = childSnapshot.key;
+            var count = childSnapshot.val();
+            countsElement.innerHTML += `<p>${name}: ${count}</p>`;
+        });
+    });
 }
 
 // Initialize display
